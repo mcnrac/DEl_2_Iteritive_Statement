d19a21b7dfef50eebba039dc0052944d35ce2308
mcnrac <mcnewr@mail.usf.edu>
2020-02-04 10:16:32 -0500

    Add project files.

---
+++ DEl_2_Iteritive_Statement/Program.cs
@@ -0,0 +1,48 @@
+﻿//Rachel McNew
+using System;
+
+namespace DEl_2_Iteritive_Statement
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {//User chooses an integer between 1-100
+            Console.WriteLine("please choose a value between 1- 100");
+
+            try 
+            {
+                string input = Console.ReadLine();
+
+                int input_value = int.Parse(input);
+                //establishing counting by 1
+                if ((input_value >= 1) && (input_value <= 100))
+                {
+                    Console.WriteLine("Executing");
+
+                    int count = 1;
+                    while (count <= input_value)
+                    {
+                        Console.WriteLine("You have entered" + input_value + "this is the current int in the loop" + " " + count);
+                        count = count + 1;
+                    }
+                }  //If the user enters a negative number
+                else if (input_value < 1)
+                { 
+                    Console.WriteLine("Enter a number within range");
+                }
+                  //If the user enters a number larger than 100
+                else if (input_value > 100)
+                {
+                    Console.WriteLine("Enter value in range");
+                }
+
+            }
+                //If the user enters a invalid integer
+            catch
+            {
+                Console.WriteLine("Enter a value that meets the requirements");
+            }
+        }
+    }
+}
